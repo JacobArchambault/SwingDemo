204a8ca199234d7842c7cd0d4b0c2749c81d5ece
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-23 10:51:33 -0400

    Remove unnecessary curly braces from addActionListener

--- src/com/jacobarchambault/swingdemo/SwingDemo.java
+++ src/com/jacobarchambault/swingdemo/SwingDemo.java
@@ -14,58 +14,30 @@ class SwingDemo extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	CardLayout cardLayout = new CardLayout();
-	CardPanel cards = new CardPanel(
-			cardLayout);
+	CardPanel cards = new CardPanel(cardLayout);
 	CenterPanel centerPanel = new CenterPanel();
-	JRadioButton[] rBs = new JRadioButton[] { new JRadioButton(
-			"Show Labels/Fields",
-			true),
-			new JRadioButton(
-					"Hide Labels/Fields",
-					false) };
+	JRadioButton[] rBs = new JRadioButton[] { new JRadioButton("Show Labels/Fields", true),
+			new JRadioButton("Hide Labels/Fields", false) };
 
-	SwingDemo(
-			final String title) {
-		super(
-				title);
-		JFrame.setDefaultLookAndFeelDecorated(
-				true);
-		setDefaultCloseOperation(
-				WindowConstants.EXIT_ON_CLOSE);
-		add(
-				cards,
-				BorderLayout.NORTH);
+	SwingDemo(final String title) {
+		super(title);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		add(cards, BorderLayout.NORTH);
 		addRadioButtonsToCenterPanel();
-		add(
-				centerPanel,
-				BorderLayout.CENTER);
-		add(
-				new BottomPanel(),
-				BorderLayout.SOUTH);
-		setSize(
-				350,
-				250);
-		setVisible(
-				true);
+		add(centerPanel, BorderLayout.CENTER);
+		add(new BottomPanel(), BorderLayout.SOUTH);
+		setSize(350, 250);
+		setVisible(true);
 	}
 
 	private void addRadioButtonsToCenterPanel() {
 		final var rBGroup = new ButtonGroup();
 		for (final JRadioButton rB : rBs) {
-			rBGroup.add(
-					rB);
-			centerPanel.add(
-					rB);
+			rBGroup.add(rB);
+			centerPanel.add(rB);
 		}
-		rBs[0].addActionListener(
-				e -> {
-					cardLayout.first(
-							cards);
-				});
-		rBs[1].addActionListener(
-				e -> {
-					cardLayout.last(
-							cards);
-				});
+		rBs[0].addActionListener(e -> cardLayout.first(cards));
+		rBs[1].addActionListener(e -> cardLayout.last(cards));
 	}
 }
